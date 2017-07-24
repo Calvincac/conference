9a9067211203d6beda02068b069712c908aa1763
Calvin Custodio <calvincac@hotmail.com>
2017-07-24 13:29:00 -0300

    Added another class

---
+++ Manager.php
@@ -0,0 +1,19 @@
+<?php
+
+class Manager
+{
+    private $talks;
+
+    public function __construct($talks)
+    {
+        $this->talks;
+    }
+
+    public function arrangeSchedule()
+    {
+        
+    }
+
+
+}
+
