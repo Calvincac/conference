5fe212b7ee27b13679b7a0f85ce8bfa59c821219
Calvin Custodio <calvincac@hotmail.com>
2017-07-27 13:26:13 -0300

    morningarrange implemented

--- Manager.php
+++ Manager.php
@@ -3,18 +3,32 @@
 class Manager
 {
     private $talks;
+    private $morningHours = 180;
+    private $afternoonHours = 240;
+    private $morningTalks = [];
 
     public function __construct($talks)
     {
         $this->talks = $talks;
+        $this->arrangeMorningSchedule();
     }
 
-    public function arrangeSchedule()
+    protected function arrangeMorningSchedule()
     {
-        foreach($this->talks as $talk) {
-            
+        $length = count($this->talks);
+        for($i=0; $i<$length; $i++) {
+            if($this->talks[$i]->getLength() <= $this->morningHours) {
+                $this->morningHours = $this->morningHours - $this->talks[$i]->getLength();
+                $this->morningTalks[] = $this->talks[$i];
+                unset($this->talks[$i]);
+            }                              
         }             
     }
 
+    public function arrangeAfternoonSchedule()
+    {
+               
+
+    }
 }
 
